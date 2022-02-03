7a938689a43d28f4040f8805f6f807085f1553fe
larry <l030411h@student.staffs.ac.uk>
2022-02-03 09:34:30 +0000

    upd: a tiny change

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,11 @@
 /*
  * @Author: your name
  * @Date: 2018-08-13 14:43:48
- * @LastEditTime: 2022-01-22 23:01:54
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-01-25 19:13:51
+ * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /finalYearProject/routes/web.php
  */
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 //welcome page route
 Route::group(['middleware'=>['web']], function () {
     Route::get('/', 'Admin\IndexController@index');
