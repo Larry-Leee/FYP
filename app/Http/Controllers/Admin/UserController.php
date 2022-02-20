a1596215f4ee9757acebc297730a42613fef1996
Larry <l030411h@student.staffs.ac.uk>
2022-02-20 23:19:18 +0000

    fix: fixed the bug of login and register module

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * @Author: Li, Hang
+ * @Date: 2022-02-20 20:45:52
+ * @LastEditors: Li, Hang
+ * @FilePath: /finalYearProject/app/Http/Controllers/Admin/UserController.php
+ */
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Models\Account;
+use Input;
+use Auth;
+use Validator;
+use Redirect;
+use Crypt;
+use Session;
+use DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //return view of adding a new user
+        return view('admin.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //1.validation rules
+        // $rules = [
+        //     'username' => 'required|min:2|max:15',
+        //     'password' => 'reuqired|min:6|max:20',
+        //     // 'email' => 'email:rfc, dns',
+        // ];
+        // //2.error messages
+        // $msg = [
+        //     'username.required' => 'Username is required!',
+        //     'username.min' => 'Username must be more than 2 characters!',
+        //     'password.required' => 'Password is required!',
+        //     'email.rfc' => 'Email is invalid!',
+        // ];
+        //3. get information and validate
+        $input = $request->except('_token');
+        $input['password'] = Crypt::encrypt($input['password']);
+        // dd($input);
+        // $validator = Validator::make(Input::all(), $rules, $msg); //validate information
+        // if ($validator->fails()) {
+        //     return Redirect::back()->withErrors($validator);
+        // }
+        $submit = Account::create(['username' => $input['username'], 'password' => $input['password'],'email' => $input['email'],'lname' => $input['lastname'], 'fname' => $input['firstname']]);
+        
+        // dd($submit);
+        if ($submit) {
+            return view('home.index');
+        } else {
+            return view('admin/login');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -94,8 +94,7 @@
     <!--login-->
 
     <ul class="login-navigation">
-        <a type="button" href="/admin/register" class=" btn btn-primary">Register</a>
-        <!-- <li data-block="#l-forget-password" class="bgm-orange">Forgot Password?</li> -->
+        <a type="button" href="{{url('admin/register/create')}}" class=" btn btn-primary">Register</a>
     </ul>
     </div>
 
@@ -139,26 +138,6 @@
             <li data-block="#l-forget-password" class="bgm-orange">Forgot Password?</li>
         </ul>
     </div>
-
-    <!-- Forgot Password -->
-    <!-- <div class="lc-block" id="l-forget-password">
-        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu risus. Curabitur commodo
-            lorem fringilla enim feugiat commodo sed ac lacus.</p>
-
-        <div class="input-group m-b-20">
-            <span class="input-group-addon"><i class="zmdi zmdi-email"></i></span>
-            <div class="fg-line">
-                <input type="text" class="form-control" placeholder="Email Address">
-            </div>
-        </div>
-
-        <a href="" class="btn btn-login btn-danger btn-float"><i class="zmdi zmdi-arrow-forward"></i></a>
-
-        <ul class="login-navigation">
-            <li data-block="#l-login" class="bgm-green">Login</li>
-            <li data-block="/admin/register" class="bgm-red">Register</li>
-        </ul>
-    </div> -->
 
     <!-- Older IE warning message -->
     <!--[if lt IE 9]>
