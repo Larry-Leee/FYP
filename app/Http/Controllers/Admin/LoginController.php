d4ec363f95ac2229c12f56d099c461c21eab44f5
Larry <l030411h@student.staffs.ac.uk>
2022-02-22 18:43:20 +0000

    upd: updated LoginController module

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-22 15:48:39
- * @LastEditTime: 2022-02-20 20:42:52
+ * @LastEditTime: 2022-02-22 01:29:25
  * @LastEditors: Li, Hang
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /finalYearProject/app/Http/Controllers/Admin/LoginController.php
--- resources/views/main/public/style.blade.php
+++ resources/views/main/public/style.blade.php
@@ -2,8 +2,8 @@
 /*
  * @Author: your name
  * @Date: 2022-01-22 15:29:20
- * @LastEditTime: 2022-01-23 00:56:42
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-02-22 00:00:50
+ * @LastEditors: Li, Hang
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /finalYearProject/resources/views/main/public/style.blade.php
  */-->
